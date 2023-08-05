8c195a8e2271090691e299e4146d77781cf7f9d6
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-08-05 12:52:41 +0200

    feat: add bidirectional relation

--- schema/schema.js
+++ schema/schema.js
@@ -1,18 +1,25 @@
-import { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } from 'graphql';
+import { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } from 'graphql';
 import axios from 'axios';
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
-    }
+        description: { type: GraphQLString },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3001/companies/${parentValue.id}/users`)
+                    .then(resp => resp.data);
+            }
+        }
+    })
 });
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
@@ -23,7 +30,7 @@ const UserType = new GraphQLObjectType({
                     .then(resp => resp.data);
             } 
         }
-    }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
